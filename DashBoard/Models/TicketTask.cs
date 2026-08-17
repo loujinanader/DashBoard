e2614c27d8eb66ff1b077d5e43f617bc66672edf
loujina.nader <loujina.nader@IT12>
2026-08-17 15:32:59 +0300

    Add TicketTask and User models; update csproj includes
    Added TicketTask and User classes to DashBoard.Models with relevant properties. Updated DashBoard.csproj to remove explicit folder inclusions for Models, ApiBroker, and Service.

---
+++ DashBoard/Models/TicketTask.cs
@@ -0,0 +1,15 @@
+﻿namespace DashBoard.Models
+{
+    public class TicketTask
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public User? UserTech { get; set; }
+        public User? GroupTech { get; set; }
+        public DateTime? Date { get; set; }
+        public DateTime? PlannedBegin { get; set; }
+        public DateTime? PlannedEnd { get; set; }
+        public int State { get; set; }
+        public int TicketsId { get; set; }
+    }
+}
