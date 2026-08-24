219a7f4a1122f5660e6fea662cc85af107a6b0e1
loujina.nader <loujina.nader@IT12>
2026-08-24 10:30:49 +0300

    Add GLPIBroker and IGlpiBroker interface implementation
    Added GLPIBroker class implementing IGlpiBroker for authenticating with the GLPI API and fetching tickets with pagination and error handling. Introduced IGlpiBroker interface for access token and ticket retrieval methods. Cleaned up DashBoard.csproj by removing an unused ItemGroup referencing ApiBroker.

---
+++ DashBoard/ApiBroker/Glpi/IGlpiBroker.cs
@@ -0,0 +1,9 @@
+﻿using DashBoard.Models.Glpi;
+namespace DashBoard.ApiBroker.Glpi
+{
+    public interface IGlpiBroker
+    {
+       public Task<string> GetAccessTokenAsync();
+       public Task<List<Ticket>> GetTicketsAsync();
+    }
+}
