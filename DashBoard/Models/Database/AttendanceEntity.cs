b42fddfdc6da6941e749908b7f0fb7a3b9058aae
loujina.nader <loujina.nader@IT12>
2026-09-03 12:10:12 +0300

    Enhance attendance models with detailed properties
    Added properties to AttendanceEntity and AttendanceRecord for detailed attendance data. Applied JsonPropertyName attributes in AttendanceRecord for JSON mapping and added System.Text.Json.Serialization using directive.

--- DashBoard/Models/Database/AttendanceEntity.cs
+++ DashBoard/Models/Database/AttendanceEntity.cs
@@ -2,5 +2,32 @@
 {
     public class AttendanceEntity
     {
+        public int Id { get; set; }
+
+        public string? EmployeeCode { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Department { get; set; }
+
+        public string? Position { get; set; }
+
+        public DateTime PunchTime { get; set; }
+
+        public string? PunchState { get; set; }
+
+        public string? PunchStateDisplay { get; set; }
+
+        public int VerifyType { get; set; }
+
+        public string? VerifyTypeDisplay { get; set; }
+
+        public string? AreaAlias { get; set; }
+
+        public string? TerminalSerialNumber { get; set; }
+
+        public string? TerminalAlias { get; set; }
     }
 }
--- DashBoard/Models/ZKBioTime/AttendanceRecord.cs
+++ DashBoard/Models/ZKBioTime/AttendanceRecord.cs
@@ -1,6 +1,49 @@
-﻿namespace DashBoard.Models.ZKBioTime
+﻿using System.Text.Json.Serialization;
+
+namespace DashBoard.Models.ZKBioTime
 {
     public class AttendanceRecord
     {
+        public int Id { get; set; }
+
+        [JsonPropertyName("emp_code")]
+        public string? EmployeeCode { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string? FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string? LastName { get; set; }
+
+        public string? Department { get; set; }
+
+        public string? Position { get; set; }
+
+        [JsonPropertyName("punch_time")]
+        public DateTime PunchTime { get; set; }
+
+        [JsonPropertyName("punch_state")]
+        public string? PunchState { get; set; }
+
+        [JsonPropertyName("punch_state_display")]
+        public string? PunchStateDisplay { get; set; }
+
+        [JsonPropertyName("verify_type")]
+        public int VerifyType { get; set; }
+
+        [JsonPropertyName("verify_type_display")]
+        public string? VerifyTypeDisplay { get; set; }
+
+        [JsonPropertyName("area_alias")]
+        public string? AreaAlias { get; set; }
+
+        [JsonPropertyName("terminal_sn")]
+        public string? TerminalSerialNumber { get; set; }
+
+        [JsonPropertyName("terminal_alias")]
+        public string? TerminalAlias
+        {
+            get; set;
+        }
     }
 }
